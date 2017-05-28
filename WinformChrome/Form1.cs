086708c9c2e26fc441fe9d1615d0729233e41bc1
iot369 <1651799671@qq.com>
2017-05-28 21:43:35 +0800

    C# winform 和谷歌浏览器

---
+++ WinformChrome/Form1.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Windows.Forms;
+using WebKit;
+
+namespace WinformChrome
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            this.WindowState = FormWindowState.Maximized;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            WebKitBrowser browser = new WebKitBrowser();
+            browser.Dock = DockStyle.Fill;//充满窗口  
+            this.Controls.Add(browser);//添加到窗口  
+            browser.Navigate("http://www.iot369.com");//跳转网页  
+        }
+    }
+}
